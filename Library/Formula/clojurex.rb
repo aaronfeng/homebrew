3ba39ae21d4dbef7a3aa4e6bf5f45f0c968f23a9
Aaron Feng <aaron.feng@gmail.com>
2010-01-26 00:57:01 -0500

    creating symlink manually from clojurex/clj to homebrew/bin

--- Library/Formula/clojurex.rb
+++ Library/Formula/clojurex.rb
@@ -12,37 +12,40 @@ class Clojurex <Formula
     cache = "#{HOMEBREW_CACHE}/#{name}-#{version}"
     cd cache
 
+    puts "#{prefix}"
     system "git submodule init"
     system "git submodule update"
     system "ant"
 
     mkdir "#{prefix}/clojure"
     mkdir "#{prefix}/clojure-contrib"
-    mkdir "#{bin}"
+    mkdir "#{prefix}/jline"
+    #mkdir "#{bin}"
 
     # prefix.install can be used if jar didn't live in sub dir
     # external scripts reference the sub dirs
-    `cp clojure/clojure.jar #{prefix}/clojure/clojure.jar` 
+    `cp clojure/clojure.jar                 #{prefix}/clojure/clojure.jar` 
+    `cp jline/jline.jar                     #{prefix}/jline/jline.jar` 
     `cp clojure-contrib/clojure-contrib.jar #{prefix}/clojure-contrib/clojure-contrib.jar` 
-    `cp configure_emacs #{prefix}/configure_emacs`
-    `cp configure_textmate #{prefix}/configure_textmate`
-    `cp generate_completions #{prefix}/generate_completions`
-    `cp clojure-completions #{prefix}/clojure-completions`
+    `cp configure_emacs                     #{prefix}/configure_emacs`
+    `cp configure_textmate                  #{prefix}/configure_textmate`
+    `cp generate_completions                #{prefix}/generate_completions`
+    `cp clojure-completions                 #{prefix}/clojure-completions`
 
     # this is nice if you want to reference swank.jar in your app
-    cd "swank-clojure"
+    cd     "swank-clojure"
     system "mvn jar:jar"
     `cp target/swank-clojure-1.0-SNAPSHOT.jar ../`
     
-    # this is necessary because clj isn't meant to run from bin/
-    `cp #{cache}/clj #{prefix}/cljx` 
-    # rename to cljx so it does collide with clojure formula
-    FileUtils.ln_s "#{prefix}/cljx", "#{bin}/cljx"
-    # uses jline console runner inside src/main/java/jline
-    `cp -r #{cache}/jline #{prefix}` 
-    `cp -r #{cache}/swank-clojure #{prefix}`
-    `cp -r #{cache}/slime #{prefix}`
-    `cp -r #{cache}/clojure-mode #{prefix}`
+    # change it to cljx so it doesn't collide with clojure formula
+    `cp    #{cache}/clj              #{prefix}` 
+    `cp -r #{cache}/swank-clojure    #{prefix}`
+    `cp -r #{cache}/slime            #{prefix}`
+    `cp -r #{cache}/clojure-mode     #{prefix}`
     `cp -r #{cache}/Clojure.tmbundle #{prefix}`
+
+    # this is a hack, creating the symlink directly into homebrew/bin
+    # so full path will be used instead of relative
+    FileUtils.ln_s "#{prefix}/clj", "#{prefix}/../../../bin/cljx"
   end
 end
